1790dee3f4a1427335a8a7dc98c511eb0d5b593c
Rob Scheepmaker <rob@ikdoe.nl>
2012-08-16 12:47:47 +0200

    fix error

--- ext/availability_cacher/availability_cacher.c
+++ ext/availability_cacher/availability_cacher.c
@@ -158,7 +158,7 @@ static void all_checkout_array_for_checkin_date( struct checkout_date_entry *dat
 						if( nights == 0 ) {
 							strncpy(desc, checkout->first[i].desc, 16);
 						} else {
-							if (strncmp(desc, "week", 16) && strncmp(checkout->first[i].desc, "week", 16)) {
+							if (strncmp(desc, "week", 16) == 0 && strncmp(checkout->first[i].desc, "week", 16) == 0) {
 								if ( (nr_nights + nights) > 7 && (nr_nights + nights) < 16 )
 									strncpy(desc, "twoweek", 16);
 								else if ( (nr_nights + nights) > 15 && (nr_nights + nights) < 23 )
